e201b90f2fb26d8cced334d474919550468b0348
Darth Vader <jose.carlos.canova@gmail.com>
2016-09-26 18:06:36 -0300

    Fixed code for the SearchUrlBuilder Custom Search

--- hubotek/src/org/hubotek/google/search/CustomSearchAdapter.java
+++ hubotek/src/org/hubotek/google/search/CustomSearchAdapter.java
@@ -8,31 +8,16 @@ import java.util.stream.Stream;
 import javax.validation.constraints.NotNull;
 
 import org.apache.log4j.Logger;
-import org.hubotek.model.rss.RssDocument;
+import org.hubotek.model.atom.AtomDocument;
 
 public class CustomSearchAdapter {
 
 	private static final Logger logger = Logger.getLogger(CustomSearchAdapter.class);
 
-	public RssDocument searchGoogle(@NotNull Map<SearchParameterEnum,String> parameters)
+	public AtomDocument searchGoogle(@NotNull String query)
 	{ 
-		String queryString = parameters.keySet().stream().flatMap(t->prepareParameter(t,parameters)).reduce(new BinaryOperator<String>(){
-			@Override
-			public String apply(String t, String u) {
-				return t + "&" + u;
-			}}).orElse("");
-		logger.debug("The Query String : " + queryString);
+		
 		return null;
 	}
-
-	private Stream<String> prepareParameter(SearchParameterEnum sp, Map<SearchParameterEnum, String> parameters) {
-		return Arrays.asList(getQueryStringParameter(sp,parameters)).stream();
-	}
-
-	private String getQueryStringParameter(SearchParameterEnum sp,
-			Map<SearchParameterEnum, String> parameters) {
-		return sp.valueOf()+"="+parameters.get(sp);
-	}
-
 
 }
